2483fb56193aaa7f28b43d05585f578a83d43349
Chris-Yan567 <moej70899@gmail.com>
2025-02-28 08:46:35 -0500

    trying first question

--- Lab5.py
+++ Lab5.py
@@ -5,8 +5,13 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
-
+for i in range(1, n + 1):
+     for j in range(1, n + 1):
+        if i == 1 or i == n or j == 1 or j == n:
+            return("*", end="")
+        else:
+             return(" ", end="")
+    return()
 # 1
 # 12
 # 123
@@ -17,7 +22,7 @@ def number_pattern(n):
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
     return ""
-
+        
 # Example for n = 4:
 #    *
 #   ***
